778375990b136e10b1907205cc556684d955a413
Professor José de Assis <josedeassisfilho@gmail.com>
2025-01-22 15:16:39 -0300

    app flex

---
+++ app.js
@@ -0,0 +1,21 @@
+/**
+ * APP - Flex
+ * @author Professor José de Assis
+ */
+
+//declaração de variáveis
+let etanol, gasolina
+
+function calcular() {
+    //entrada de dados(input)
+    etanol = frmFlex.inputEtanol.value
+    gasolina = frmFlex.inputGasolina.value
+    //console.log(etanol)
+    //console.log(gasolina)
+    if (etanol < 0.7 * gasolina) {
+        document.getElementById('status').src="img/etanol.png"
+    } else {
+        document.getElementById('status').src="img/gasolina.png"
+    }
+    return false
+}
